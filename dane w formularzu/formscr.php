9ff97bd5a683d9c6c754238eb74a170acfa41919
Kciuk <135543040+KrzLena@users.noreply.github.com>
2024-01-24 08:25:46 +0100

    Add files via upload

---
+++ dane w formularzu/formscr.php
@@ -0,0 +1,10 @@
+<?php //you can use get
+echo "You've send following information:<br>";
+echo "Name:      ".$_POST['name']."<br>";
+echo "Surname:   ".$_POST['surname']."<br>";
+echo "Age:       ".$_POST['age']."<br>";
+echo "E-mail:    ".$_POST['mail']."<br>";
+echo "Education: ".$_POST['e']."<br>";
+echo "Consent:   ".$_POST['agree']."<br>";
+echo "Language:  ".$_POST['lang']."<br>";
+?>
