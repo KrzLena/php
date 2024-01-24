69d740e7d06f55dac2a72e3ca04cd01437ad20cd
Kciuk <135543040+KrzLena@users.noreply.github.com>
2024-01-24 08:45:30 +0100

    Update formscr.php

--- dane w formularzu/formscr.php
+++ dane w formularzu/formscr.php
@@ -6,5 +6,18 @@ echo "Age:       ".$_POST['age']."<br>";
 echo "E-mail:    ".$_POST['mail']."<br>";
 echo "Education: ".$_POST['e']."<br>";
 echo "Consent:   ".$_POST['agree']."<br>";
-echo "Language:  ".$_POST['lang']."<br>";
+//case 0 will whrite only one lang
+//echo "Language:  ".$_POST['lang']."<br>"; 
+
+//case 1 not that good
+//echo "Language:  ".$_POST['lang'][1].",".$_POST['lang'][2]."<br>"; 
+
+//case 2 is pretty good
+echo "Language: ";
+echo "<ul>";
+foreach($_POST['lang'] as $j)
+{
+    echo "<li>".$j."</li>";
+}
+echo "</ul>";
 ?>
