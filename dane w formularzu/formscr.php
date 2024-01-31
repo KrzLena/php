e6c329dd4020dc6ab4c5e1298d1bea469e374941
Kciuk <135543040+KrzLena@users.noreply.github.com>
2024-01-31 08:41:49 +0100

    U_2.php

--- dane w formularzu/formscr.php
+++ dane w formularzu/formscr.php
@@ -1,23 +1,90 @@
 <?php //you can use get
-echo "You've send following information:<br>";
-echo "Name:      ".$_POST['name']."<br>";
-echo "Surname:   ".$_POST['surname']."<br>";
-echo "Age:       ".$_POST['age']."<br>";
-echo "E-mail:    ".$_POST['mail']."<br>";
-echo "Education: ".$_POST['e']."<br>";
-echo "Consent:   ".$_POST['agree']."<br>";
+echo "You've send following information: <br><br>";
+
+//----------------------------------------------------------------------------------name nad surname:
+if(empty($_POST['name']) || empty($_POST['surname']))
+{
+    echo "<p style = 'color:red'>you have not entered basic information</p>";
+}
+else
+{
+    echo "Name:      ".$_POST['name']."<br>";
+    echo "Surname:   ".$_POST['surname']."<br>";
+}
+
+//----------------------------------------------------------------------------------age:
+
+
+//check if their underage
+if($_POST['age']<18)
+{
+    echo "<p style = 'color:red'>!You are underage!</p>";
+}
+else
+{
+    echo "Age:       ".$_POST['age']."<br>";
+}
+
+//----------------------------------------------------------------------------------email:
+if(empty($_POST['mail']))
+{
+    echo "<p style = 'color:red'>!You didn't provide your e-mail!</p>";
+}
+else
+{
+    echo "E-mail: ".$_POST['mail']."<br>";
+}
+
+
+//----------------------------------------------------------------------------------education:
+//echo "Education: ".$_POST['e']."<br>";
+if(isSet($_POST['e'])) //if edu isnt choosen it will return the message 
+{
+    echo "Education - ".$_POST['e']."<br>";
+}
+else
+{
+    echo "<p style = 'color:red'>!You haven't chosen your education level!</p>";
+}
+
+//----------------------------------------------------------------------------------consnet:
+if(empty($_POST['agree']))
+{
+    echo "<p style = 'color:red'>!You haven't consent to the processing of my personal data!</p>";
+}
+else
+{
+    echo "Consent:   ".$_POST['agree']."<br>";
+}
+
+
 //case 0 will whrite only one lang
 //echo "Language:  ".$_POST['lang']."<br>"; 
 
 //case 1 not that good
 //echo "Language:  ".$_POST['lang'][1].",".$_POST['lang'][2]."<br>"; 
 
-//case 2 is pretty good
-echo "Language: ";
-echo "<ul>";
-foreach($_POST['lang'] as $j)
+//----------------------------------------------------------------------------------language:
+if(empty($_POST['lang'])) //if the lan is empty then the message is shown
 {
-    echo "<li>".$j."</li>";
+    echo"<p style = 'color:red'>!You haven't chosen a language!</p>";
 }
-echo "</ul>";
+else
+{
+    echo "Language: "; //case 2 is pretty good
+    echo "<ul>";
+    foreach($_POST['lang'] as $j)
+    {
+        echo "<li>".$j."</li>";
+    }
+    echo "</ul>";
+}
+
+//----------------------------------------------------------------------------------file1:
+
+echo "File data:<br>";
+echo "File name: ".$_FILES['file1']['name'];
+echo "Temporary location: ".$_FILES['file1']['tmp_name'];
+echo "File size: ".$_FILES['file1']['size'];
+echo "file type: ".$_FILES['file1']['type'];
 ?>
